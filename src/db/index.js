e0e646f99f6210d6ca8c664956e932a14a4ddfef
darshan <darshanhiteshshah@gmail.com>
2025-04-13 13:28:19 +0530

    util code discuss

--- src/db/index.js
+++ src/db/index.js
@@ -10,3 +10,9 @@ const connectDB=async () => {
     }
 }
 export default connectDB
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => console.log(`Server running on port ${process.env.PORT}`));
+})
+.catch((err)=>{
+    console.log(`mongo db connection failed:`, err)
+})
